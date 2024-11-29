afddddfeee4fdcfb86ea5a1bb86a0a2ca137d0fa
jhowrf <jhowrf@gmail.com>
2024-11-29 09:11:03 -0300

    feat: crete restrict base tables view

--- application/views/restrict.php
+++ application/views/restrict.php
@@ -30,19 +30,101 @@
             </div>
             <div class="container">
                 <ul class="nav nav-tabs">
-                    <li class="active"><a href="#tab_courses" role="tab" data-toggle="tab">Cursos</a></li>
-                    <li><a href="#tab_team" role="tab" data-toggle="tab">Equipe</a></li>
-                    <li><a href="#tab_user" role="tab" data-toggle="tab">Usuários</a></li>
+                    <li class="active">
+                        <a href="#tab_courses" role="tab" data-toggle="tab">
+                            Cursos
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#tab_team" role="tab" data-toggle="tab">
+                            Equipe
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#tab_user" role="tab" data-toggle="tab">
+                            Usuários
+                        </a>
+                    </li>
                 </ul>
                 <div class="tab-content">
                     <div id="tab_courses" class="tab-pane active">
-                        Curses
+                        <div class="container-fluid">
+                            <h2 class="text-center">
+                                <strong>
+                                    Gerenciar Cursos
+                                </strong>
+                            </h2>
+                            <a id="btn_add_course" class="btn btn-primary" style="margin-bottom: .99em;">
+                                <i class="fa fa-plus"></i>
+                                &nbsp;&nbsp;Adicionar
+                            </a>
+                            <table id="dt_courses" class="table table-striped table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>Nome</th>
+                                        <th>Imagem</th>
+                                        <th>Duração</th>
+                                        <th>Descrição</th>
+                                        <th>Ações</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                     <div id="tab_team" class="tab-pane">
-                        Equipes
+                        <div class="container-fluid">
+                            <h2 class="text-center">
+                                <strong>
+                                    Gerenciar Equipe
+                                </strong>
+                            </h2>
+                            <a id="btn_add_member" class="btn btn-primary" style="margin-bottom: .99em;">
+                                <i class="fa fa-plus"></i>
+                                &nbsp;&nbsp;Adicionar
+                            </a>
+                            <table id="dt_team" class="table table-striped table-bordered">
+                                <thead>
+                                <tr>
+                                    <th>Nome</th>
+                                    <th>Foto</th>
+                                    <th>Descrição</th>
+                                    <th>Ações</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                     <div id="tab_user" class="tab-pane">
-                        Usuários
+                        <div class="container-fluid">
+                            <h2 class="text-center">
+                                <strong>
+                                    Gerenciar Usuários
+                                </strong>
+                            </h2>
+                            <a id="btn_add_user" class="btn btn-primary" style="margin-bottom: .99em;">
+                                <i class="fa fa-plus"></i>
+                                &nbsp;&nbsp;Adicionar
+                            </a>
+                            <table id="dt_users" class="table table-striped table-bordered">
+                                <thead>
+                                <tr>
+                                    <th>Login</th>
+                                    <th>Nome</th>
+                                    <th>E-mail</th>
+                                    <th>Ações</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
