c291d8238dc54d2717e19a373fea0f926850736a
jhowrf <jhowrf@gmail.com>
2024-11-30 10:18:16 -0300

    feat: crete team and courses model

---
+++ application/models/CoursesModel.php
@@ -0,0 +1,47 @@
+<?php
+
+class CoursesModel extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function get_data(int $id, string $select = null)
+    {
+        if (!empty($select)) {
+            $this->db->select($select);
+        }
+        $this->db->from('courses');
+        $this->db->where('course_id', $id);
+        return $this->db->get();
+    }
+
+    public function insert($data)
+    {
+        $this->db->insert('courses', $data);
+    }
+
+    public function update(int $id, $data)
+    {
+        $this->db->where('course_id', $id);
+        $this->db->update('courses', $data);
+    }
+
+    public function delete(int $id, string $select = null)
+    {
+        $this->db->where('course_id', $id);
+        $this->db->delete('courses');
+    }
+
+    public function is_duplicated($field, $value, $id = null): bool
+    {
+        if (!empty($id)) {
+            $this->db->where('course_id <>', $id);
+        }
+        $this->db->from('courses');
+        $this->db->where($field, $value);
+        return $this->db->get()->num_rows() > 0;
+    }
+}
